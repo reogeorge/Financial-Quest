61c5afa363051a6a724665b7228a766d38f30f29
Sourav U P <souravputhanveedu@gmail.com>
2024-04-20 20:01:50 +0530

    Pages Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,7 @@
-import { useEffect, useState } from "react";
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-  "https://uvybqulkppefqiugpljz.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV2eWJxdWxrcHBlZnFpdWdwbGp6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM2MTA5NDMsImV4cCI6MjAyOTE4Njk0M30.fL7HpdnrH3aQAsKAZuJetV1tpUbSktoC9GPPuN_7xBk"
-);
+import Login from "./pages/Login/Login";
 
 function App() {
-  const [countries, setCountries] = useState([]);
-
-  useEffect(() => {
-    getCountries();
-  }, []);
-
-  async function getCountries() {
-    const { data } = await supabase.from("countries").select();
-    setCountries(data);
-  }
-
-  return (
-    <ul>
-      {countries.map((country) => (
-        <li key={country.name}>{country.name}</li>
-      ))}
-    </ul>
-  );
+  return <Login />;
 }
 
 export default App;
