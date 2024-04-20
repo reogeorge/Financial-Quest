a628b8bb3fc96fcd80b1ea142262ff4fc339c84b
Sourav U P <souravputhanveedu@gmail.com>
2024-04-20 22:13:57 +0530

    Login Error Correction

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,5 +1,5 @@
 import styles from "./Login.module.css";
-import heroImg from "./Assets/heroImage";
+import heroImg from "../../assets/loginImg.svg";
 function Login() {
   return (
     <div className={styles.container}>
