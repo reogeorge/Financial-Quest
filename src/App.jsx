6c3602d549b1baf0d076cc14108cad9d1a3d5328
JERISHAB <jerishabmayan@gmail.com>
2024-04-20 23:09:41 +0700

    login edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import './App.css';
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/Login';
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,19 +3,26 @@ import heroImg from "../../assets/loginImg.svg";
 function Login() {
   return (
     <div className={styles.container}>
-      <div className={styles.heroText}>HeroText</div>
-      <div className={styles.heroImage}>
+        <div className={styles.heroText}>FINANCIAL QUEST</div>
         <img src={heroImg} />
-      </div>
-      <div className={styles.subText}>
-        <div className={styles.line} />
-        <div className={styles.loginText}>Login</div>
-        <div className={styles.line} />
-      </div>
-      <div className={styles.username}></div>
-      <div className={styles.password}></div>
-      <div className={styles.button}></div>
-      <div className={styles.forgotText}></div>
+        <div className={styles.subText}>
+            <div className={styles.line} />
+            <div className={styles.loginText}>LOGIN</div>
+            <div className={styles.line} />
+        </div>
+        <div className={styles.username}>
+            <label className="Username">USERNAME</label>
+            <input type="text" placeholder="Username" className={styles.input} />
+        </div>
+        <div className={styles.username}>
+            <label className="Username">PASSWORD</label>
+            <input type="text" placeholder="Password" className={styles.input} />
+        </div>
+        <div className={styles.button}>
+            <button className={styles.loginButton}>Login</button>
+            <h3 className={styles.or}>sign up instead</h3>
+        </div>
+        <div className={styles.forgotText}></div>
     </div>
   );
 }
