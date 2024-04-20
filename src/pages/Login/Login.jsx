ac05565e691b85007c4c9c752e866fd78fe1a36b
Sourav U P <souravputhanveedu@gmail.com>
2024-04-20 21:42:32 +0530

    Login Structure

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,22 @@
 import styles from "./Login.module.css";
-
+import heroImg from "./Assets/heroImage";
 function Login() {
   return (
-    <>
-      <div className={styles.hero}>Hello</div>
-      <div className={styles.heroImage}>Hello</div>
-    </>
+    <div className={styles.container}>
+      <div className={styles.heroText}>HeroText</div>
+      <div className={styles.heroImage}>
+        <img src={heroImg} />
+      </div>
+      <div className={styles.subText}>
+        <div className={styles.line} />
+        <div className={styles.loginText}>Login</div>
+        <div className={styles.line} />
+      </div>
+      <div className={styles.username}></div>
+      <div className={styles.password}></div>
+      <div className={styles.button}></div>
+      <div className={styles.forgotText}></div>
+    </div>
   );
 }
 
