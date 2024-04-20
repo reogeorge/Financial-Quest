4144384d50b358f479e3cf98d31d10c42b18e009
JERISHAB <jerishabmayan@gmail.com>
2024-04-20 21:39:14 +0700

    routing complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
 import React from "react";
-import Login from "./pages/Login/Login";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import Achievements from './pages/Achievements/Achievements';
+import Game from './pages/Game/Game';
+import Home from './pages/Home/Home';
+import Learning from './pages/Learning/Learning';
+import NotFound from './pages/NotFound/NotFound';
+import SignUP from './pages/SignUp/SignUp';
+import Welcome from './pages/Welcome/Welcome';
 
 function App() {
-  return <Login />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/achievements" element={<Achievements />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/learning" element={<Learning />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/signup" element={<SignUP />} />
+        <Route path="/welcome" element={<Welcome />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
