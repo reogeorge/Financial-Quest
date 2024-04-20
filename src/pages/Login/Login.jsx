2b1d8548f4ac100f7672fda86e461d114f16f968
JERISHAB <jerishabmayan@gmail.com>
2024-04-20 23:57:06 +0700

    login edited

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,28 +2,30 @@ import styles from "./Login.module.css";
 import heroImg from "../../assets/loginImg.svg";
 function Login() {
   return (
-    <div className={styles.container}>
-        <div className={styles.heroText}>FINANCIAL QUEST</div>
-        <img src={heroImg} />
-        <div className={styles.subText}>
-            <div className={styles.line} />
-            <div className={styles.loginText}>LOGIN</div>
-            <div className={styles.line} />
+    <div className={styles.outercontainer}>
+        <div className={styles.container}>
+            <div className={styles.heroText}>FINANCIAL QUEST</div>
+            <img src={heroImg} />
+            <div className={styles.subText}>
+                <div className={styles.line} />
+                <div className={styles.loginText}>LOGIN</div>
+                <div className={styles.line} />
+            </div>
+            <div className={styles.username}>
+                <label className={styles.user}>USERNAME</label>
+                <input type="text" placeholder="Username" className={styles.input} />
+            </div>
+            <div className={styles.username}>
+                <label className={styles.user}>PASSWORD</label>
+                <input type="text" placeholder="Password" className={styles.input} />
+            </div>
+            <div className={styles.button}>
+                <button className={styles.loginButton}>Login</button>
+                <h3 className={styles.or}>sign up instead</h3>
+            </div>
+            <h4 className={styles.forgot}>oops i forgot my password</h4>
         </div>
-        <div className={styles.username}>
-            <label className="Username">USERNAME</label>
-            <input type="text" placeholder="Username" className={styles.input} />
-        </div>
-        <div className={styles.username}>
-            <label className="Username">PASSWORD</label>
-            <input type="text" placeholder="Password" className={styles.input} />
-        </div>
-        <div className={styles.button}>
-            <button className={styles.loginButton}>Login</button>
-            <h3 className={styles.or}>sign up instead</h3>
-        </div>
-        <div className={styles.forgotText}></div>
-    </div>
+    </div>    
   );
 }
 
